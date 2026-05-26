bebd0ed1016215bc23d0b982ef8d73f9f61ed2c1
Luis <jorge10258@gmail.com>
2026-05-26 00:08:45 -0500

    Se agrego los potenciales barrera y escalon

--- src/potenciales.py
+++ src/potenciales.py
@@ -4,3 +4,11 @@ def pozo_infinito(x,limite_izq,limite_der,valor_infinito=1e10):
     V=np.full_like(x,valor_infinito,dtype=np.float64)
     V[(x>=limite_izq)&(x<=limite_der)]=0.0
     return V 
+def escalon(x,V_0):
+    V=np.zeros_like(x)
+    V[(x>=0)]=V_0 
+    return V
+def barrera(x,limite_izq,limite_der,V_0):
+    V=np.zeros_like(x)
+    V[(x>=limite_izq)&(x<=limite_der)]=V_0
+    return V

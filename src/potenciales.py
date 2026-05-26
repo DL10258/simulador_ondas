03732e25ece406ae9c8d6a7c37cfefde47948c6d
Luis <jorge10258@gmail.com>
2026-05-25 23:53:47 -0500

    Se definio el pozo potencial infinito

---
+++ src/potenciales.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+def pozo_infinito(x,limite_izq,limite_der,valor_infinito=1e10):
+    V=np.full_like(x,valor_infinito,dtype=np.float64)
+    V[(x>=limite_izq)&(x<=limite_der)]=0.0
+    return V 

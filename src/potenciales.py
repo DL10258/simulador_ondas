2b548e2191f39fa99eef9fa86f17b842f9004e4f
Luis <jorge10258@gmail.com>
2026-05-26 22:04:53 -0500

    Se agrego el parametro kwargs
    Para evitar futuros conflictos en los argumentos de los potenciales, se
    agrega **kwargs al final del argumento de todos los potenciales.

--- src/potenciales.py
+++ src/potenciales.py
@@ -1,18 +1,18 @@
 import numpy as np 
 
-def pozo_infinito(x,limite_izq,limite_der,valor_infinito=1e10):
+def pozo_infinito(x,limite_izq,limite_der,valor_infinito=1e10,**kwargs):
     V=np.full_like(x,valor_infinito,dtype=np.float64)
     V[(x>=limite_izq)&(x<=limite_der)]=0.0
     return V 
-def escalon(x,punto_potencial,V_0):
+def escalon(x,punto_potencial,V_0,**kwargs):
     V=np.zeros_like(x)
     V[(x>=punto_potencial)]=V_0 
     return V
-def barrera(x,limite_izq,limite_der,V_0):
+def barrera(x,limite_izq,limite_der,V_0,**kwargs):
     V=np.zeros_like(x)
     V[(x>=limite_izq)&(x<=limite_der)]=V_0
     return V
-def pozo_finito(x,limite_izq,limite_der,V_0):
+def pozo_finito(x,limite_izq,limite_der,V_0,**kwargs):
     V=np.zeros_like(x)
     V[(x>=limite_izq)&(x<=limite_der)]=-np.abs(V_0)
     return V 

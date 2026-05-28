7d956dc602b8e258f73697d3a459cfe89786f519
Luis <jorge10258@gmail.com>
2026-05-27 21:22:57 -0500

    Limpieza
    Se decidio tener el main lo mas limpio posible.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,2 @@
-import numpy as np 
-from solver import paso_tiempo
-
-def obtener_datos():
-    print("INGRESAR DATOS PARA LA SIMULACIÓN\n")
-    datos={}
-    datos["limite_izq"]=float(input("Limite izquierdo: "))
-    datos["limite_der"]=float(input("Limite derecho: "))
-    datos["v0"]=float(input("V0: "))
-    return datos 
-user_config=obtener_datos()
-
-
-
+from userprefs import *
+from potenciales import *

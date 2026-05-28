0df4142eb5869211b1572ff23792a33f7b2af0d4
Luis <jorge10258@gmail.com>
2026-05-28 12:34:25 -0500

    Se hizo un arreglo temporal al potencial escalon
    Se reordeno el potencial escalon, de manera que ahora puede ser elegido,
    a futuro se me ocurre que cambiar los argumentos de todas las funciones
    para que sean (np.linspace(a,b,N),dict(),**kwargs) se ve necesario para
    el branch main.

--- src/potenciales.py
+++ src/potenciales.py
@@ -1,13 +1,18 @@
 import numpy as np 
-
+def libre(x,**kwargs):
+    V=np.zeros_like(x)
+    return V
 def pozo_infinito(x,limite_izq,limite_der,valor_infinito=1e10,**kwargs):
     V=np.full_like(x,valor_infinito,dtype=np.float64)
     V[(x>=limite_izq)&(x<=limite_der)]=0.0
     return V 
-def escalon(x,punto_potencial,V_0,**kwargs):
-    V=np.zeros_like(x)
-    V[(x>=punto_potencial)]=V_0 
-    return V
+def escalon(x,limite_izq,limite_der,V_0,punto_potencial,**kwargs):
+    if limite_izq<=limite_der:
+        V=np.zeros_like(x)
+        V[(x>=punto_potencial)]=V_0 
+        return V
+    else:
+        return np.zeros_like(x)
 def barrera(x,limite_izq,limite_der,V_0,**kwargs):
     V=np.zeros_like(x)
     V[(x>=limite_izq)&(x<=limite_der)]=V_0

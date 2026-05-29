d2e06be80605a4aa97e8c2fe84bab3a166a6d14a
Luis <jorge10258@gmail.com>
2026-05-29 18:52:28 -0500

    Se define el potencial absorbente
    Creacion del potencial absorbente para el main

--- src/potenciales.py
+++ src/potenciales.py
@@ -1,4 +1,12 @@
 import numpy as np 
+def agregar_absorbente(V, x, ancho=3.0, fuerza=15.0):
+    V_abs = V.astype(complex)
+    x_min, x_max = x[0], x[-1]
+    mask_izq = x < (x_min + ancho)
+    mask_der = x > (x_max - ancho)
+    V_abs[mask_izq] -= 1j * fuerza * ((x_min + ancho - x[mask_izq]) / ancho)**2
+    V_abs[mask_der] -= 1j * fuerza * ((x[mask_der] - (x_max - ancho)) / ancho)**2
+    return V_abs
 def libre(x,**kwargs):
     V=np.zeros_like(x)
     return V
